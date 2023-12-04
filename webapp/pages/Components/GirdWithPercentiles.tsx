73fe98ab4ffbc312ea38ea662ce002df1ae9c00b
Nathan_Dinh <dinhtrongnhan2015@gmail.com>
2023-12-04 13:40:07 -0500

    Reorganize school info cards to visually group student and faculty related data together

--- webapp/pages/Components/GirdWithPercentiles.tsx
+++ webapp/pages/Components/GirdWithPercentiles.tsx
@@ -29,14 +29,14 @@ const GridWithPercentileCircles: React.FC<GridWithPercentileCirclesProps> = ({
     <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
       {dropoutRate !== undefined && <TitleWithPercentileCircle title="Dropout Rate" percentile={dropoutRate} />}
       {expendituresPerStudent !== undefined && <TitleWithPercentileCircle title="Expenditures Per Student" percentile={expendituresPerStudent} />}
-      {studentToFacultyRatio !== undefined && <TitleWithPercentileCircle title="Student to Faculty Ratio" percentile={studentToFacultyRatio} />}
-      {avgTeacherEducationLevel !== undefined && <TitleWithPercentileCircle title="Avg Teacher Education Level" percentile={avgTeacherEducationLevel} />}
-
       {collegeBound !== undefined && <TitleWithPercentileCircle title="Percent College Bound" percentile={collegeBound} />}
       {lowIncome !== undefined && <TitleWithPercentileCircle title="Percent Low Income" percentile={lowIncome} />}
+
+      {studentToFacultyRatio !== undefined && <TitleWithPercentileCircle title="Student to Faculty Ratio" percentile={studentToFacultyRatio} />}
+      {avgTeacherEducationLevel !== undefined && <TitleWithPercentileCircle title="Avg Teacher Education Level" percentile={avgTeacherEducationLevel} />}
       {facultyDegree !== undefined && <TitleWithPercentileCircle title="Avg Faculty Degree" percentile={facultyDegree} />}
       {avgTeacherExperience !== undefined && <TitleWithPercentileCircle title="Avg Faculty Experience" percentile={avgTeacherExperience} />}
-      { avgTeacherSalary !== undefined && <TitleWithPercentileCircle title="Avg Faculty Salary" percentile={avgTeacherSalary} />}
+      {avgTeacherSalary !== undefined && <TitleWithPercentileCircle title="Avg Faculty Salary" percentile={avgTeacherSalary} />}
     </div>
   );
 };
